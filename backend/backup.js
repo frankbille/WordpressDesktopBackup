9b52d4d34f4ac576f57b0bafb9d19602c4d56be5
Frank Bille <github@frankbille.dk>
2015-07-30 01:22:31 +0200

    Now I have a working prototype.
    It actually takes a backup of posts, comments, taxonomy, users and
    media (plus files). There are room for plenty of performance
    improvements though.

--- backend/backup.js
+++ backend/backup.js
@@ -1,132 +1,188 @@
 var Q = require('q');
-var Request = require('superagent');
-var Request2 = require('request');
+var Request = require('request');
 var Path = require('path');
 var Jetpack = require('fs-jetpack');
-var Crypto = require('crypto');
 var Moment = require('moment');
 var Fs = require('fs');
+var Async = require('async');
 
 module.exports = {
   backup: function(options, config, siteConfig) {
-    var backupDateString = Moment().format('YYYY-MM-DDTHH-mm-ss');
-    var blogFolder = Path.join(config.backupLocation, siteConfig.blog_id);
-    var backupFolder = Path.join(blogFolder, backupDateString);
+    this.options = options;
+    this.config = config;
+    this.siteConfig = siteConfig;
+    this.backupDateString = Moment().format('YYYY-MM-DDTHH-mm-ss');
+    this.blogFolder = Path.join(config.backupLocation, siteConfig.blog_id);
+    this.backupFolder = this.blogFolder;
+    this.request = Request.defaults({
+      json: true,
+      headers: {
+        Authorization: 'BEARER ' + siteConfig.access_token
+      }
+    });
 
     var thisBackup = this;
     return thisBackup
-      ._getBackupMeta(blogFolder)
+      ._getBackupMeta()
       .then(function(backupMeta) {
-        return Q.allSettled([
-          thisBackup._backupPosts(options, siteConfig, backupFolder,
-            backupMeta),
-          thisBackup._backupComments(options, siteConfig,
-            backupFolder,
-            backupMeta),
-          thisBackup._backupMedia(options, siteConfig, backupFolder,
-            backupMeta),
-          thisBackup._backupUsers(options, siteConfig, backupFolder,
-            backupMeta),
-          thisBackup._backupCategories(options, siteConfig,
-            backupFolder, backupMeta),
-          thisBackup._backupTags(options, siteConfig, backupFolder,
-            backupMeta)
-        ]).then(function(results) {
-          return thisBackup._processBackup(blogFolder, backupFolder,
-            backupMeta);
-        }).then(function() {
-          backupMeta.lastBackup = backupDateString;
-          return thisBackup._saveBackupMeta(blogFolder, backupMeta);
-        });
+        thisBackup.backupMeta = backupMeta;
+
+        return thisBackup._backupPosts()
+          .then(function() {
+            return thisBackup._backupComments();
+          })
+          .then(function() {
+            return thisBackup._backupUsers();
+          })
+          .then(function() {
+            return thisBackup._backupCategories();
+          })
+          .then(function() {
+            return thisBackup._backupTags();
+          })
+          .then(function() {
+            return thisBackup._backupMedia();
+          })
+          .then(function(mediaEntries) {
+            return thisBackup._backupMediaFiles(mediaEntries);
+          })
+          .then(function() {
+            backupMeta.lastBackup = thisBackup.backupDateString;
+            return thisBackup._saveBackupMeta();
+          });
       });
   },
 
-  _backupPosts: function(options, siteConfig, backupFolder, backupMeta, repo) {
-    return this._doBackup(siteConfig, backupFolder, backupMeta,
-      1, 'posts', options.createApiUrl('sites/' + siteConfig.blog_id +
-        '/posts'));
+  _backupPosts: function() {
+    return this._doBackup('posts');
   },
 
-  _backupComments: function(options, siteConfig, backupFolder, backupMeta,
-    repo) {
-    return this._doBackup(siteConfig, backupFolder, backupMeta,
-      1, 'comments', options.createApiUrl('sites/' + siteConfig.blog_id +
-        '/comments'));
+  _backupComments: function() {
+    return this._doBackup('comments');
   },
 
-  _backupMedia: function(options, siteConfig, backupFolder, backupMeta, repo) {
-    return this._doBackup(siteConfig, backupFolder, backupMeta,
-      1, 'media', options.createApiUrl('sites/' + siteConfig.blog_id +
-        '/media'), this._downloadMediaEntry);
+  _backupUsers: function() {
+    return this._doBackup('users');
   },
 
-  _downloadMediaEntry: function(backupFolder, mediaEntry, siteConfig) {
-    var mediaFolder = Path.join(backupFolder, 'media');
-    Jetpack.dir(mediaFolder);
-    var mediaFilePath = Path.join(mediaFolder, mediaEntry.file);
-    var fileStream = Fs.createWriteStream(mediaFilePath);
-    fileStream.on('finish', function() {
-      fileStream.close();
+  _backupCategories: function() {
+    return this._doBackup('categories');
+  },
+
+  _backupTags: function() {
+    return this._doBackup('tags');
+  },
+
+  _backupMedia: function() {
+    return this._doBackup('media', function(mediaEntry) {
+      return {
+        file: mediaEntry.file,
+        url: mediaEntry.URL
+      }
     });
-    Request
-      .get(mediaEntry.URL)
-      .set('Authorization', 'BEARER ' + siteConfig.access_token)
-      .pipe(fileStream);
   },
 
-  _backupUsers: function(options, siteConfig, backupFolder, backupMeta, repo) {
-    return this._doBackup(siteConfig, backupFolder, backupMeta,
-      1, 'users', options.createApiUrl('sites/' + siteConfig.blog_id +
-        '/users'));
-  },
-
-  _backupCategories: function(options, siteConfig, backupFolder, backupMeta,
-    repo) {
-    return this._doBackup(siteConfig, backupFolder, backupMeta,
-      1, 'categories', options.createApiUrl('sites/' + siteConfig.blog_id +
-        '/categories'));
-  },
-
-  _backupTags: function(options, siteConfig, backupFolder, backupMeta, repo) {
-    return this._doBackup(siteConfig, backupFolder, backupMeta,
-      1, 'tags', options.createApiUrl('sites/' + siteConfig.blog_id +
-        '/tags'));
-  },
-
-  _doBackup: function(siteConfig, backupFolder, backupMeta, page, type, url,
-    entryCallback) {
-    if (typeof(backupMeta[type]) !== 'object') {
-      backupMeta[type] = {};
+  _backupMediaFiles: function(mediaEntries) {
+    if (typeof(this.backupMeta.mediaFiles) !== 'object') {
+      this.backupMeta.mediaFiles = {};
     }
 
     var thisBackup = this;
     return Q.promise(function(resolve, reject) {
-      Request
-        .get(url)
-        .set('Authorization', 'BEARER ' + siteConfig.access_token)
-        .query({
-          page: page,
-          number: 100
+      var mediaFolder = Path.join(thisBackup.backupFolder, 'media');
+      Jetpack.dir(mediaFolder);
+
+      var queue = Async.queue(function(task, callback) {
+        doNotify();
+
+        if (task.lastModified) {
+          thisBackup.request
+            .head(task.url, {}, function(err, response) {
+              if (err) {
+                callback(err);
+              } else {
+                if (response.headers['last-modified'] != task.lastModified) {
+                  task.lastModified = false;
+                  queue.push(task);
+                }
+                callback();
+              }
+            });
+        } else {
+          var fileStream = Fs.createWriteStream(task.filePath);
+          fileStream.on('finish', function() {
+            fileStream.close();
+            callback();
+          });
+          thisBackup.request
+            .get(task.url)
+            .on('response', function(response) {
+              thisBackup.backupMeta.mediaFiles[task.file] =
+                response.headers['last-modified'];
+            })
+            .pipe(fileStream);
+        }
+      }, 10);
+
+      var doNotify = function() {
+        console.log(mediaEntries.length, queue.length(), queue.running());
+      };
+
+      queue.drain = function() {
+        doNotify();
+        resolve();
+      };
+
+      mediaEntries.forEach(function(mediaEntry) {
+        queue.push({
+          file: mediaEntry.file,
+          filePath: Path.join(mediaFolder, mediaEntry.file),
+          url: mediaEntry.url,
+          lastModified: thisBackup.backupMeta.mediaFiles[
+            mediaEntry.file]
         })
-        .end(function(err, res) {
+      });
+    });
+  },
+
+  _doBackup: function(type, entryProcessor) {
+    var blogId = this.siteConfig.blog_id;
+    var url = this.options.createApiUrl('sites/' + blogId + '/' + type);
+
+    var thisBackup = this;
+    return this._downloadData(url, type, 1, entryProcessor, [])
+  },
+
+  _downloadData: function(url, type, page, entryProcessor, result) {
+    console.log('Backing up', type, 'page', page);
+
+    var thisBackup = this;
+    return Q.promise(function(resolve, reject) {
+      thisBackup.request
+        .get(url, {
+          qs: {
+            page: page,
+            number: 100
+          }
+        }, function(error, response, data) {
           thisBackup
-            ._saveData(err, res.body[type], type, backupFolder,
-              backupMeta, siteConfig, entryCallback)
+            ._saveData(data[type], type, result, entryProcessor)
             .then(function() {
-              if (res.body[type].length >= 100) {
-                return thisBackup._doBackup(siteConfig,
-                  backupFolder, backupMeta, page + 1, type, url,
-                  entryCallback);
+              if (data[type].length >= 100) {
+                return thisBackup._downloadData(url, type, page + 1,
+                  entryProcessor, result);
               }
             })
-            .then(resolve)
+            .then(function() {
+              resolve(result);
+            })
             .catch(reject);
         });
     });
   },
 
-  _getBackupMeta: function(blogFolder) {
-    var backupMetaFile = Path.join(blogFolder, 'backup.json');
+  _getBackupMeta: function() {
+    var backupMetaFile = Path.join(this.blogFolder, 'backup.json');
     return Jetpack
       .readAsync(backupMetaFile, 'jsonWithDates')
       .then(function(backupMeta) {
@@ -137,51 +193,29 @@ module.exports = {
       })
   },
 
-  _saveBackupMeta: function(blogFolder, backupMeta) {
-    var backupMetaFile = Path.join(blogFolder, 'backup.json');
+  _saveBackupMeta: function() {
+    var backupMetaFile = Path.join(this.blogFolder, 'backup.json');
+    var thisBackup = this;
     return Q.promise(function(resolve) {
-      Jetpack.write(backupMetaFile, backupMeta);
-      resolve(backupMeta);
+      Jetpack.write(backupMetaFile, thisBackup.backupMeta);
+      resolve(thisBackup.backupMeta);
     });
   },
 
-  _saveData: function(err, dataArray, type, backupFolder, backupMeta,
-    siteConfig, entryCallback) {
+  _saveData: function(dataArray, type, result, entryProcessor) {
     var thisBackup = this;
     return Q.promise(function(resolve, reject) {
-      if (err) {
-        reject(new Error(err));
-      } else {
-        dataArray.forEach(function(data) {
-          var dataString = JSON.stringify(data, null, 4);
+      dataArray.forEach(function(data) {
+        var fileName = type + '-' + data.ID + '.json';
+        var filePath = Path.join(thisBackup.backupFolder,
+          fileName);
+        Jetpack.write(filePath, data);
 
-          var fileName = type + '-' + data.ID + '.json';
-          var filePath = Path.join(backupFolder, fileName);
-
-          backupMeta[type][fileName] = thisBackup._sha(dataString);
-          Jetpack.write(filePath, dataString);
-
-          if (typeof(entryCallback) === 'function') {
-            entryCallback(backupFolder, data, siteConfig);
-          }
-        });
-        resolve();
-      }
+        if (typeof(entryProcessor) === 'function') {
+          result.push(entryProcessor(data));
+        }
+      });
+      resolve(result);
     });
-  },
-
-  _processBackup: function(blogFolder, backupFolder, backupMeta) {
-    return Q.promise(function(resolve, reject) {
-      if (typeof(backupMeta.lastBackup) === 'string') {
-        var lastBackupFolder = Path.join(blogFolder, backupMeta.lastBackup);
-      }
-      resolve();
-    });
-  },
-
-  _sha: function(text) {
-    var shasum = Crypto.createHash('sha1');
-    shasum.update(text);
-    return shasum.digest('hex');
   }
 }
